2b4cd4d011e77e89aa12afdac9de25f6185b0a56
Van Huynh Le <v.h.le@protonmail.com>
2023-11-04 18:01:50 +0100

    Add explanation about exception in constructor

--- item_08_stack.hpp
+++ item_08_stack.hpp
@@ -6,8 +6,11 @@ template <typename T>
 class Stack {
 public:
     /// 1. This function is exception neutral: We don't catch anything.
-    /// 2. We don't leak: If `new` fails and throws std::bad_alloc,
-    /// nothing has been allocated yet.
+    /// 2. We don't leak:
+    ///    2.1 If `new` throws std::bad_alloc, nothing has been allocated yet.
+    ///    2.2 If `new` can allocate the memory, but one of T's default constructor throws,
+    ///        any constructed T's objects will be destructed. Assuming that T's destructor
+    ///        does not throw, then the objects will be cleaned up correctly.
     /// 3. We're always in a consistent state. If an exception is thrown
     /// here, no object is created yet, so there's nothing to be inconsistent.
     Stack()
