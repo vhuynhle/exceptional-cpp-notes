5a3c7c94ffe4d4ea00d4199b81ea94e943d6f223
Van Huynh Le <v.h.le@protonmail.com>
2023-11-04 18:34:59 +0100

    Use a common Stack.hpp file

--- Stack.hpp
+++ Stack.hpp
@@ -3,10 +3,20 @@
 #include <algorithm>
 #include <cassert>
 #include <cstddef>
+#include <stdexcept>
+#include <utility>
 
 template <typename T>
 class Stack {
 public:
+    // 1. This function is exception neutral: We don't catch anything.
+    // 2. We don't leak:
+    //    2.1 If `new` throws std::bad_alloc, nothing has been allocated yet.
+    //    2.2 If `new` can allocate the memory, but one of T's default constructor throws,
+    //        any constructed T's objects will be destructed. Assuming that T's destructor
+    //        does not throw, then the objects will be cleaned up correctly.
+    // 3. We're always in a consistent state. If an exception is thrown
+    // here, no object is created yet, so there's nothing to be inconsistent.
     Stack()
         : v_ { new T[10] }
         , vsize_ { 10 }
@@ -16,6 +26,7 @@ public:
 
     ~Stack() noexcept
     {
+        // This delete[] operator is noexcept
         delete[] v_;
     }
 
@@ -69,6 +80,44 @@ public:
 
     Stack(Stack&&) = default;
     Stack& operator=(Stack&&) = default;
+
+    std::size_t Count() const
+    {
+        return vused_;
+    }
+
+    void Push(const T& obj)
+    {
+        // Re-allocate if needed
+        if (vused_ == vsize_) {
+            const auto new_size = 2 * vsize_;
+            T* tmp = NewCopy(v_, vsize_, new_size);
+
+            // Delete old stack, this will not throw
+            delete[] v_;
+
+            // Take the new contents
+            v_ = tmp;
+            vsize_ = new_size;
+        }
+
+        v_[vused_] = obj;
+    }
+
+    T Pop()
+    {
+        if (vused_ == 0) {
+            throw std::out_of_range("Empty stack");
+        }
+
+        // Take the content of the top item
+        Stack res { std::move(v_[vused_ - 1]) };
+
+        // Updating the size
+        --vused_;
+
+        return res;
+    }
 
 private:
     T* v_;
